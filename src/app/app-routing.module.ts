7d295414509248cd188e16df77853820cba9a04d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-11-30 19:13:47 +0300

    import missing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './about/about.component';
 import { UsersComponent } from './users/users.component';
 import { TenantsComponent } from './tenants/tenants.component';
 import { RolesComponent } from 'app/roles/roles.component';
+import { CasesComponent } from './cases/cases.component';
 
 @NgModule({
     imports: [
