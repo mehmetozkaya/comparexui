bce030f1d26ec39d24425430f11284cf92c1cfee
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-04 14:25:54 +0300

    case detail

--- src/app/cases/case-detail/case-detail.component.ts
+++ src/app/cases/case-detail/case-detail.component.ts
@@ -1,11 +1,10 @@
 import { AppComponentBase } from "@shared/app-component-base";
 import { OnInit, Injector } from "@angular/core";
-import { CaseDetailOutput, CaseServiceProxy, EntityDtoOfGuid, CaseRegistrationDto, CaseRegisterOutput } from "@shared/service-proxies/service-proxies";
+import { CaseDetailOutput, CaseServiceProxy, EntityDtoOfGuid, CaseRegisterOutput } from "@shared/service-proxies/service-proxies";
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { __createInputTransfer } from "@angularclass/hmr";
-import { AbpHttpInterceptor } from "abp-ng2-module/dist/src/abpHttpInterceptor";
 
-
+import * as _ from "lodash";
 
 
 export class CaseDetailComponent extends AppComponentBase implements OnInit {
@@ -35,16 +34,50 @@ export class CaseDetailComponent extends AppComponentBase implements OnInit {
 
         this._caseService.registerAsync(input)
             .subscribe((result: CaseRegisterOutput) => {
-                abp.notify.success('Successfully registered to event. Your registration id: ' + result.registrationId + ".");
+                abp.notify.success('Successfully registered to case. Your registration id: ' + result.registrationId + ".");
                 this.loadCase();
-            })
+            });
     }
+
+    cancelRegistrationFromCase(): void {
+        var input = new EntityDtoOfGuid();
+        input.id = this.case.id;
+
+        this._caseService.cancelRegistrationAsync(input)
+            .subscribe(() => {
+                abp.notify.info('Canceled your registration.');
+                this.loadCase();
+            });
+    };
+
+    cancelCase(): void {
+        var input = new EntityDtoOfGuid();
+        input.id = this.case.id;
+
+        this._caseService.cancelAsync(input)
+            .subscribe(() => {
+                abp.notify.info('Canceled the case.');
+                this.backToCasesPage();
+            });
+    };
+
+    isRegistered(): boolean {
+        return _.some(this.case.registrations, { userId: abp.session.userId });
+    };
+
+    isCaseCreator(): boolean {
+        return this.case.creatorUserId === abp.session.userId;
+    };
 
     loadCase(): void {
         this._caseService.getDetailAsync(this.caseId)
             .subscribe((result: CaseDetailOutput) => {
                 this.case = result;
             });
-    }
+    };
+
+    backToCasesPage() {
+        this._router.navigate(['app/cases']);
+    };
 
 }
