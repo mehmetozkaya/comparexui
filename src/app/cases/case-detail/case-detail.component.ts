82a7c224468a0839b6a2d93a1b20a831ab4b6005
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-12-04 14:16:47 +0300

    registerToCase

--- src/app/cases/case-detail/case-detail.component.ts
+++ src/app/cases/case-detail/case-detail.component.ts
@@ -1,7 +1,9 @@
 import { AppComponentBase } from "@shared/app-component-base";
 import { OnInit, Injector } from "@angular/core";
-import { CaseDetailOutput, CaseServiceProxy } from "@shared/service-proxies/service-proxies";
+import { CaseDetailOutput, CaseServiceProxy, EntityDtoOfGuid, CaseRegistrationDto, CaseRegisterOutput } from "@shared/service-proxies/service-proxies";
 import { Router, ActivatedRoute, Params } from "@angular/router";
+import { __createInputTransfer } from "@angularclass/hmr";
+import { AbpHttpInterceptor } from "abp-ng2-module/dist/src/abpHttpInterceptor";
 
 
 
@@ -25,6 +27,24 @@ export class CaseDetailComponent extends AppComponentBase implements OnInit {
             this.caseId = params['caseId'];
             this.loadCase();
         });
+    }
+
+    registerToCase(): void {
+        var input = new EntityDtoOfGuid();
+        input.id = this.case.id;
+
+        this._caseService.registerAsync(input)
+            .subscribe((result: CaseRegisterOutput) => {
+                abp.notify.success('Successfully registered to event. Your registration id: ' + result.registrationId + ".");
+                this.loadCase();
+            })
+    }
+
+    loadCase(): void {
+        this._caseService.getDetailAsync(this.caseId)
+            .subscribe((result: CaseDetailOutput) => {
+                this.case = result;
+            });
     }
 
 }
